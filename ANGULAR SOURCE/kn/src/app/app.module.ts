67e3653c99eda82b289a9f9bd913c4b7988aefa1
Trond Albinussen <tal@computas.com>
2020-09-25 04:36:30 +0200

    No Material yet.

--- ANGULAR SOURCE/kn/src/app/app.module.ts
+++ ANGULAR SOURCE/kn/src/app/app.module.ts
@@ -4,20 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from '@core/core.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule.forRoot(),
-    BrowserAnimationsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
