729e8545d204f6ae685ac366969f28df58653bf1
Trond Albinussen <tal@computas.com>
2020-09-25 02:43:30 +0200

    Added angular material

--- ANGULAR SOURCE/kn/src/app/app.module.ts
+++ ANGULAR SOURCE/kn/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from '@core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CoreModule } from '@core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
