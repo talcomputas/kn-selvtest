badb3247c3aa9a128a7986331f455ee45f05f9e3
Trond Albinussen <tal@computas.com>
2020-10-13 14:33:37 +0200

    Environments for reading test

--- ANGULAR SOURCE/kn/src/environments/environment.lesetesten.ts
+++ ANGULAR SOURCE/kn/src/environments/environment.lesetesten.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  qbIdNb: 'ejelvlehqp',
-  qbIdNn: 'hqivwkebty',
+  qbIdNb: 'wzy4jqo0hw',
+  qbIdNn: '562ajj0nzs',
   STATISTICS_API: 'http://vox-wp2.vox.no/dataoppsamling/lesetest',
 };
