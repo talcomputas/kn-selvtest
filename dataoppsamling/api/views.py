03f5a9fbaf47936b52c467c279f3d25158705f14
Øystein Hegglid Follo <ohf@computas.com>
2020-11-06 14:39:32 +0100

    First get from itemdata

--- dataoppsamling/api/views.py
+++ dataoppsamling/api/views.py
@@ -5,6 +5,8 @@ from flask.json import jsonify
 from . import app
 from .connect import connection
 
+import json
+
 
 @app.route("/")
 def home():
@@ -74,3 +76,24 @@ def submitItem():
 
     return jsonify(id=id), 201
 
+@app.route("/api/itemdata", methods=["GET"])
+def itemdata():
+    token = secrets.token_hex(18)
+
+    cursor, conn = connection()
+    query = cursor.execute("USE kompetanse_norge_regnesjek; SELECT * from itemdata LIMIT 3", multi=True)
+
+    result = None
+
+    for cur in query:
+        print("cursor", cur)
+        if cur.with_rows:
+            result = [dict((cur.description[i][0], value)
+                for i, value in enumerate(row)) for row in cur.fetchall()]
+
+    print(result)
+
+    conn.commit()
+    conn.close()
+
+    return jsonify(result), 201
